1f7c6f5c094f7cb2ad276fdbe2c8eb16c9416087
gabrielquadro <61526044+gabrielquadro@users.noreply.github.com>
2020-03-25 17:29:57 -0300

    Alteração dos tamanho, distancia das table roes e remoção da barra de cima e deixando full screen

--- app/src/main/java/com/unisc/ex2_2/MainActivity.java
+++ app/src/main/java/com/unisc/ex2_2/MainActivity.java
@@ -2,13 +2,19 @@ package com.unisc.ex2_2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
     }
 }
